15560d3ca6df455a32fc1572fcfab87b048368bf
root <ladefoged1991@gmail.com>
2018-04-17 13:00:25 +0200

    setup

--- Exercise_11_csharp/file_client/file_client.cs
+++ Exercise_11_csharp/file_client/file_client.cs
@@ -28,7 +28,20 @@ namespace Application
 		/// </param>
 	    private file_client(String[] args)
 	    {
-	    	// TO DO Your own code
+			/*
+			System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient ();
+			//opretter sockets
+			Console.WriteLine("client made");
+
+			//connecting
+			clientSocket.Connect(args[0].ToString(),PORT);
+			Console.WriteLine("connected to server");
+
+			//find networkstream
+			NetworkStream serverstreamIO = clientSocket.GetStream ();
+			Console.WriteLine ("made networkstream");
+			receiveFile (args[1].ToString(), serverstreamIO);
+			*/
 	    }
 
 		/// <summary>
@@ -43,6 +56,46 @@ namespace Application
 		private void receiveFile (String fileName, Transport transport)
 		{
 			// TO DO Your own code
+			/*
+			 			Int64 fileSize = 0;
+			//make byte array
+			Byte[] buff = new byte[BUFSIZE];
+
+			//tell what file we want
+			tcp.LIB.writeTextTCP (io, fileName);
+		
+
+			//modtag fil størrelse
+			fileSize = Int64.Parse( tcp.LIB.readTextTCP(io));
+			Console.WriteLine ("size is " + fileSize);
+
+
+			//modtag fil
+			FileStream fs = new FileStream(fileName,FileMode.OpenOrCreate,FileAccess.Write);
+
+			Int32 bytesReceived = 0;
+			Int64 totalbytedReceived = 0;
+			Int64 megaByte = 1048576;
+			while ((bytesReceived = io.Read (buff,0,buff.Length))>0) {
+				totalbytedReceived += bytesReceived;
+				fs.Write (buff, 0, bytesReceived);
+				int percentCompleted = (int)Math.Round(((double)(totalbytedReceived/(double)fileSize)*100));
+				Console.Write("\r{0} ", "Received: " + totalbytedReceived/megaByte + " Mbytes" + " Out of " + fileSize/megaByte + " Mbytes" + " total: " +  percentCompleted + " %");
+			}
+			if (totalbytedReceived > 0) {
+				Console.WriteLine("You have received a file! congratulations!");
+			}
+			else{
+				Console.WriteLine ("Sadly you did not receive a file :( ");
+			}
+
+
+
+			io.Close ();
+			fs.Close ();
+
+
+			 */
 		}
 
 		/// <summary>
@@ -53,6 +106,7 @@ namespace Application
 		/// </param>
 		public static void Main (string[] args)
 		{
+			Console.WriteLine ("Client starts...");
 			new file_client(args);
 		}
 	}
--- Exercise_11_csharp/file_server/file_server.cs
+++ Exercise_11_csharp/file_server/file_server.cs
@@ -14,12 +14,57 @@ namespace Application
 		private const int BUFSIZE = 1000;
 		private const string APP = "FILE_SERVER";
 
+		/*		
+		IPAddress localAddr = IPAddress.Parse("10.0.0.1");
+		byte[] buff = new byte[BUFSIZE];
+		*/
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="file_server"/> class.
 		/// </summary>
 		private file_server ()
 		{
 			// TO DO Your own code
+			/*
+			//opret socket
+			TcpListener serverSocket = new TcpListener (localAddr, PORT);
+			TcpClient clientSocket = default(TcpClient);
+			serverSocket.Start();
+			Console.WriteLine("Server started");
+			while (true) {
+				//wait for client
+				clientSocket = serverSocket.AcceptTcpClient ();
+				Console.WriteLine ("client connected");
+
+				//opretter en stream fra client
+				NetworkStream serverStreamIO = clientSocket.GetStream (); 
+				Console.WriteLine (" >> Accepted connection from client");
+
+				//modtager filnavn
+				string fileDir; //= @"/root/Desktop/IKNServerClientTCP/Exercise_6_server/file_server/bin/Debug/files/";
+				string userfile = tcp.LIB.readTextTCP (serverStreamIO);
+
+				fileDir = userfile;
+				//check for exsitens af fil
+				long lengthOfFile = tcp.LIB.check_File_Exists (fileDir);
+
+				if (lengthOfFile != 0) {//filen findes
+					Console.WriteLine ("filen findes " + fileDir);
+					//find størrelsen på filen
+					//long filesize = new System.IO.FileInfo (fileDir).Length;
+					long filesize = LIB.check_File_Exists(fileDir);
+					//send the file
+					sendFile (fileDir, filesize, serverStreamIO);
+				} else { //filen exsitere ikke
+					Console.WriteLine ("Filen findes ikke " + fileDir);
+					tcp.LIB.writeTextTCP (serverStreamIO, "filen findes ikke");
+				}
+				//clientSocket.Close ();
+			}
+
+
+			serverSocket.Stop();
+			*/
 		}
 
 		/// <summary>
@@ -37,6 +82,35 @@ namespace Application
 		private void sendFile(String fileName, long fileSize, Transport transport)
 		{
 			// TO DO Your own code
+			/*//send filstørelse
+			tcp.LIB.writeTextTCP(io,fileSize.ToString());
+
+			FileStream fs = new FileStream (fileName, FileMode.Open, FileAccess.Read);
+
+			int numberOfPackages = Convert.ToInt32 (Math.Ceiling (Convert.ToDouble (fileSize) / Convert.ToDouble (BUFSIZE)));
+			long currentPacketLength = 0;
+			long totalLength = fileSize;
+
+			//write out
+			for (int i = 0; i < numberOfPackages; i++) {
+				if (totalLength > BUFSIZE) {
+					currentPacketLength = BUFSIZE;
+					totalLength -= BUFSIZE;
+	
+
+				} else {
+
+					currentPacketLength = totalLength;
+				}
+
+				byte[] sendingBuffer = new byte[currentPacketLength];
+
+				fs.Read (sendingBuffer, 0, (int)currentPacketLength);
+				io.Write (sendingBuffer, 0, sendingBuffer.Length);
+			}
+
+			fs.Close ();
+			io.Close ();*/
 		}
 
 		/// <summary>
@@ -47,6 +121,7 @@ namespace Application
 		/// </param>
 		public static void Main (string[] args)
 		{
+			Console.WriteLine ("Server starts...");
 			new file_server();
 		}
 	}
